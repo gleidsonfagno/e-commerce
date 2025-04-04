aac46ec5568e81ae65e9300a4466be33bf9be68d
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-04-03 21:35:09 -0300

    feate: Ajustando menu para a abertura e fechamento e no clique das categorias

--- src/js/menu-toggle.js
+++ src/js/menu-toggle.js
@@ -1,15 +1,45 @@
 const button_menu = document.querySelector(".menu-toggle")
-const content_menu = document.querySelector(".container-menu");
+const li_buttoon = document.querySelector(".menu-toggle-li")
+const content_menu = document.querySelector(".container-menu")
+
+const category_links = document.querySelector(".category-links")
+const paragraph_category = document.querySelector(".category")
 
 let menu = false
 
 button_menu.addEventListener("click", () => {
-    console.log("deucerto")
+
     if(!menu){
         content_menu.style.display = "block";
+        category_links.style.display = "flex"
+        paragraph_category.style.display = "none"
+        menu = true
+        
+    }else {
+        content_menu.style.display = "none";
+        menu = false
+    }
+})
+
+li_buttoon.addEventListener("click", () => {
+
+    if(!menu){
+        content_menu.style.display = "block";
+        category_links.style.display = "none"
+        paragraph_category.style.display = "flex"
         menu = true
     }else {
         content_menu.style.display = "none";
         menu = false
     }
 })
+
+window.addEventListener("resize", () => {
+    console.log("Nova largura:", window.innerWidth);
+    console.log("Nova altura:", window.innerHeight);
+
+    if (window.innerWidth <= 768){
+        content_menu.style.display = "none";
+        return menu = false
+    }
+  });
