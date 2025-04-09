0843b8dc2f4b6fe381280eca73c7262ccba0a24e
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-04-09 09:56:57 -0300

    feat: Criando a função de toggle junto com o rotação do icone no footer

--- src/js/faq-toggle.js
+++ src/js/faq-toggle.js
@@ -1,8 +1,11 @@
-const question = document.querySelectorAll(".faq-question")
+const question = document.querySelectorAll(".faq-question");
 
-question.forEach(question => {
-    question.addEventListener("click", () => {
-        const answer = question.nextElementSibling;
-    answer.classList.toggle('open');
-    })
-})
+question.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    const svgIcon = question.querySelector('.faq-question span');
+
+    answer.classList.toggle("open");
+    svgIcon.classList.toggle("rotated")
+  });
+});
